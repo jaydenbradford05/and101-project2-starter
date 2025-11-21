98e3512225f5db81ce145c83e643846871b3dddc
jaydenbradford05 <jaydenbradford05@gmail.com>
2025-11-21 15:31:33 -0500

    Update PrintListActivity.kt

--- app/src/main/java/com/driuft/androidkotlindebugging/ui/PrintListActivity.kt
+++ app/src/main/java/com/driuft/androidkotlindebugging/ui/PrintListActivity.kt
@@ -26,9 +26,9 @@ class PrintListActivity : AppCompatActivity() {
 
     private fun combinedWords(): String {
         var combined = ""
-        for (idx in 0 until words.size + 1) {
-            combined += "${words[idx]}\n"
-        }
+        for (idx in words.indices) {
+    combined += "${words[idx]}\n"
+}
         return combined
     }
 }
